0c34e30059eda15a55a18308af2e502657fbcfc4
刘楠楠 <liunannan@qianxin.com>
2022-04-07 11:07:28 +0800

    style: 格式修改

--- bin/stupid-project-doc.js
+++ bin/stupid-project-doc.js
@@ -3,24 +3,24 @@
 const commander = require('commander');
 const program = new commander.Command();
 program
-  .version(require('../package.json').version)
-  .usage("<command>")
+	.version(require('../package.json').version)
+	.usage('<command>');
 
 program
-  .command('dev <sourceDir>')
-  .description('start development server')
-  .option('-n, --no-nav', 'not automatically generate navigation')
-  .action((...args) => {
-    require('../lib/commands/dev.js')(...args);
-  })
+	.command('dev <sourceDir>')
+	.description('start development server')
+	.option('-n, --no-nav', 'not automatically generate navigation')
+	.action((...args) => {
+		require('../lib/commands/dev.js')(...args);
+	});
 
 program
-  .command('build <sourceDir>')
-  .description('build dir as static site')
-  .option('-n, --no-nav', 'not automatically generate navigation')
-  .option('-o, --output <outputDir>', 'product output target loaction', 'docs')
-  .action((...args) => {
-    require('../lib/commands/build.js')(...args);
-  });
+	.command('build <sourceDir>')
+	.description('build dir as static site')
+	.option('-n, --no-nav', 'not automatically generate navigation')
+	.option('-o, --output <outputDir>', 'product output target loaction', 'docs')
+	.action((...args) => {
+		require('../lib/commands/build.js')(...args);
+	});
 
 program.parse(process.argv);
